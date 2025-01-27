6b8d937150ef621a24bf8e88cb5d5ebc66998680
Erik Johansson <erik.elias.johansson@gmail.com>
2025-01-27 12:16:14 +0100

    sidebar

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
 import streamlit as st
 from plot_maps import TripMap
 
@@ -6,6 +10,8 @@ from utils.constants import StationIds
 trip_map = TripMap(
     origin_id=StationIds.MALMO.value, destination_id=StationIds.UMEA.value
 )
+
+img_path = Path(__file__).parent / "images"
 
 if "stage" not in st.session_state:
     st.session_state.stage = 0
@@ -22,7 +28,25 @@ def set_search(start, end):
     st.session_state.search = f"{start}, {end}"
 
 
+test_df = pd.DataFrame(np.random.randn(10, 2), columns=(["Linje", "Avgår om (min)"]))
+
+
 def main():
+    st.sidebar.header("Tidstabell")
+
+    timetable_input = st.sidebar.text_input(
+        "Sök avgångar", placeholder="Stad/Hållplats/Station"
+    )
+
+    st.sidebar.dataframe(
+        test_df.set_index(test_df.columns[0]),
+        use_container_width=True,
+        on_select="rerun",
+        selection_mode="single-row",
+    )
+
+    st.sidebar.markdown(timetable_input)
+
     st.markdown(
         """<span style='
         color: #20265A;
@@ -33,7 +57,6 @@ def main():
     )
 
     if st.session_state.stage == 0:
-        st.markdown("# Reseplanerare")
         time_table, travel_planner = st.columns(2)
         time_table.button(
             "Tidstabell", use_container_width=True, on_click=set_mode, args=[1]
@@ -41,13 +64,14 @@ def main():
         travel_planner.button(
             "Reseplanerare", use_container_width=True, on_click=set_mode, args=[2]
         )
-        st.markdown(
+        desc = st.container(border=True)
+        desc.markdown(
             (
-                "Den här dashboarden syftar till att både utforska data för olika "
-                "platser, men ska även fungera som en reseplanerare där du får "
-                "välja och planera din resa."
+                "Den här dashboarden syftar till att både utforska data för olika platser, men ska även fungera som en reseplanerare där du får välja och planera din resa."  # noqa: E501
             )
         )
+        image = st.container(border=True)
+        image.image(img_path / "Gronsakstorget_1800x1800.jpg")
 
     if st.session_state.stage == 1:
         title, middle, switch = st.columns([0.6, 0.1, 0.3], vertical_alignment="bottom")
@@ -59,8 +83,7 @@ def main():
         desc = st.container(border=True)
         desc.markdown(
             (
-                "Här visas tider för kommande resor till "
-                "och från en specifierad hållplats/station."
+                "Här visas tider för kommande resor till och från en specifierad hållplats/station."  # noqa: E501
             )
         )
         checks = st.columns(7)
@@ -92,7 +115,8 @@ def main():
         st.button("Sök resa", on_click=set_search(start_name, end_name))
         if st.session_state.search != "default":
             st.markdown(st.session_state.search)
-        trip_map.display_map()
+        with st.expander("Visa på karta", icon=":material/map:"):
+            trip_map.display_map()
 
 
 if __name__ == "__main__":
